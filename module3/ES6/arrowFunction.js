50f8cbe4721b8a2bc5e8ed63bf6a50a1d44bee7d
srinibas <srinibaskhuntia39@gmail.com>
2021-08-26 21:33:52 +0530

    Arrow functions

---
+++ module3/ES6/arrowFunction.js
@@ -0,0 +1,14 @@
+// function f(x,y){
+//     console.log("something")
+//     return 2*x+y;
+// }
+
+let f=(x,y)=>{
+    console.log("something")
+    return 2*x+y;
+}
+let f1=x=>{
+    console.log("something");
+    return 2*x;
+}
+let f1=x=>2*x;
