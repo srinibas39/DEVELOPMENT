66181576c44ca5cfd1436390d87068490bbc2aef
srinibas <srinibaskhuntia39@gmail.com>
2021-10-08 13:20:23 +0530

    partail UI of shopping cart done

---
+++ module3/shoppingcart/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Product from "./Product";
+import "./Home.css"
+let Home=()=>{
+   return(
+       <>
+       <div className="product-components">
+       <Product/>
+       <Product/>
+       <Product/>
+
+       </div>
+       </>
+   )
+}
+export default Home;
---
+++ module3/shoppingcart/src/components/Product.jsx
@@ -0,0 +1,17 @@
+import "./Product.css";
+
+let Product = () => {
+  return <>
+   <div className="product">
+       <div className="product-img">
+         <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1536184191i/41721428._UY500_SS500_.jpg" alt=""></img>
+       </div>
+       <div className="product-button">
+         <button>Add to Cart</button>
+       </div>
+   </div>
+  
+  
+  </>;
+};
+export default Product;
